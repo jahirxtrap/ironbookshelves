13b684e11d69b568592db9e3beee89a8e7f7225b
jahirxtrap <jahirroman2002@gmail.com>
2023-09-13 01:17:35 -0500

    More bookshelves (Close #2), configurable values, fireproof

---
+++ fabric/src/main/java/com/jahirtrap/ironbookshelves/init/IronbookshelvesModConfig.java
@@ -0,0 +1,30 @@
+package com.jahirtrap.ironbookshelves.init;
+
+import eu.midnightdust.lib.config.MidnightConfig;
+
+public class IronbookshelvesModConfig extends MidnightConfig {
+    @Entry(name = "Iron Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    public static double ironEnchantPower = 2.0;
+    @Entry(name = "Golden Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    public static double goldenEnchantPower = 3.0;
+    @Entry(name = "Diamond Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    public static double diamondEnchantPower = 5.0;
+    @Entry(name = "Emerald Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    public static double emeraldEnchantPower = 6.0;
+    @Entry(name = "Obsidian Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    public static double obsidianEnchantPower = 7.0;
+    @Entry(name = "Netherite Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    public static double netheriteEnchantPower = 10.0;
+    @Entry(name = "Copper Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    public static double copperEnchantPower = 1.5;
+    @Entry(name = "Amethyst Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    public static double amethystEnchantPower = 2.5;
+    @Entry(name = "Crying Obsidian Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    public static double cryingObsidianEnchantPower = 8.0;
+    @Entry(name = "Enderite Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    public static double enderiteEnchantPower = 12.0;
+    @Entry(name = "Steel Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    public static double steelEnchantPower = 3.0;
+    @Entry(name = "Bronze Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    public static double bronzeEnchantPower = 4.0;
+}
---
+++ forge/src/main/java/com/jahirtrap/ironbookshelves/block/CryingObsidianBookshelfBlock.java
@@ -0,0 +1,36 @@
+package com.jahirtrap.ironbookshelves.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.PushReaction;
+
+import java.util.Random;
+
+import static com.jahirtrap.ironbookshelves.init.IronbookshelvesModConfig.CRYING_OBSIDIAN_ENCHANT_POWER;
+
+public class CryingObsidianBookshelfBlock extends BaseBookshelfBlock {
+    public CryingObsidianBookshelfBlock() {
+        super(SoundType.STONE, 50f, 1200f, CRYING_OBSIDIAN_ENCHANT_POWER.get(), 3, 10, PushReaction.BLOCK);
+    }
+
+    @Override
+    public void animateTick(BlockState state, Level level, BlockPos pos, Random rand) {
+        if (rand.nextInt(5) == 0) {
+            Direction direction = Direction.getRandom(rand);
+            if (direction != Direction.UP) {
+                BlockPos blockpos = pos.relative(direction);
+                BlockState blockstate = level.getBlockState(blockpos);
+                if (!state.canOcclude() || !blockstate.isFaceSturdy(level, blockpos, direction.getOpposite())) {
+                    double d0 = direction.getStepX() == 0 ? rand.nextDouble() : 0.5D + (double) direction.getStepX() * 0.6D;
+                    double d1 = direction.getStepY() == 0 ? rand.nextDouble() : 0.5D + (double) direction.getStepY() * 0.6D;
+                    double d2 = direction.getStepZ() == 0 ? rand.nextDouble() : 0.5D + (double) direction.getStepZ() * 0.6D;
+                    level.addParticle(ParticleTypes.DRIPPING_OBSIDIAN_TEAR, (double) pos.getX() + d0, (double) pos.getY() + d1, (double) pos.getZ() + d2, 0.0D, 0.0D, 0.0D);
+                }
+            }
+        }
+    }
+}
