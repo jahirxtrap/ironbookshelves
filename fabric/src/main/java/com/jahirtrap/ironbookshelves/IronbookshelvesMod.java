9643518793d5aa7d1b4db3531cf524bad6a1e697
jahirxtrap <jahirroman2002@gmail.com>
2024-07-30 20:02:23 -0500

    Update environment

--- fabric/src/main/java/com/jahirtrap/ironbookshelves/IronbookshelvesMod.java
+++ fabric/src/main/java/com/jahirtrap/ironbookshelves/IronbookshelvesMod.java
@@ -2,7 +2,7 @@ package com.jahirtrap.ironbookshelves;
 
 import com.jahirtrap.configlib.TXFConfig;
 import com.jahirtrap.ironbookshelves.init.ModConfig;
-import com.jahirtrap.ironbookshelves.init.ModItems;
+import com.jahirtrap.ironbookshelves.init.ModContent;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -15,13 +15,13 @@ public class IronbookshelvesMod implements ModInitializer {
     @Override
     public void onInitialize() {
         TXFConfig.init(MODID, ModConfig.class);
-        ModItems.init();
+        ModContent.init();
         buildContents();
     }
 
     public void buildContents() {
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(event -> {
-            event.addAfter(Items.BOOKSHELF, ModItems.IRON_BOOKSHELF, ModItems.GOLDEN_BOOKSHELF, ModItems.DIAMOND_BOOKSHELF, ModItems.EMERALD_BOOKSHELF, ModItems.OBSIDIAN_BOOKSHELF, ModItems.NETHERITE_BOOKSHELF, ModItems.COPPER_BOOKSHELF, ModItems.AMETHYST_BOOKSHELF, ModItems.CRYING_OBSIDIAN_BOOKSHELF, ModItems.ENDERITE_BOOKSHELF, ModItems.STEEL_BOOKSHELF, ModItems.BRONZE_BOOKSHELF);
+            event.addAfter(Items.BOOKSHELF, ModContent.IRON_BOOKSHELF, ModContent.GOLDEN_BOOKSHELF, ModContent.DIAMOND_BOOKSHELF, ModContent.EMERALD_BOOKSHELF, ModContent.OBSIDIAN_BOOKSHELF, ModContent.NETHERITE_BOOKSHELF, ModContent.COPPER_BOOKSHELF, ModContent.AMETHYST_BOOKSHELF, ModContent.CRYING_OBSIDIAN_BOOKSHELF, ModContent.ENDERITE_BOOKSHELF, ModContent.STEEL_BOOKSHELF, ModContent.BRONZE_BOOKSHELF);
         });
     }
 }
