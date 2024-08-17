566a91b60171c143bed14e6381baf2af19285098
jahirxtrap <jahirroman2002@gmail.com>
2024-08-17 01:23:15 -0500

    Mixin, registry

--- forge/src/main/java/com/jahirtrap/ironbookshelves/IronbookshelvesMod.java
+++ forge/src/main/java/com/jahirtrap/ironbookshelves/IronbookshelvesMod.java
@@ -3,7 +3,9 @@ package com.jahirtrap.ironbookshelves;
 import com.jahirtrap.ironbookshelves.init.ModConfig;
 import com.jahirtrap.ironbookshelves.init.ModContent;
 import com.jahirtrap.ironbookshelves.util.configlib.TXFConfig;
+import net.minecraft.world.item.CreativeModeTab.TabVisibility;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -11,8 +13,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-
-import static com.jahirtrap.ironbookshelves.util.CommonUtils.putAfter;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod(IronbookshelvesMod.MODID)
 public class IronbookshelvesMod {
@@ -32,7 +33,11 @@ public class IronbookshelvesMod {
 
     public void buildContents(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
-            putAfter(event, Items.BOOKSHELF, ModContent.IRON_BOOKSHELF, ModContent.GOLDEN_BOOKSHELF, ModContent.DIAMOND_BOOKSHELF, ModContent.EMERALD_BOOKSHELF, ModContent.OBSIDIAN_BOOKSHELF, ModContent.NETHERITE_BOOKSHELF, ModContent.COPPER_BOOKSHELF, ModContent.AMETHYST_BOOKSHELF, ModContent.CRYING_OBSIDIAN_BOOKSHELF, ModContent.ENDERITE_BOOKSHELF, ModContent.STEEL_BOOKSHELF, ModContent.BRONZE_BOOKSHELF);
+            var prev = Items.BOOKSHELF;
+            for (RegistryObject<Item> item : ModContent.ITEMS.getEntries()) {
+                event.getEntries().putAfter(prev.getDefaultInstance(), item.get().getDefaultInstance(), TabVisibility.PARENT_AND_SEARCH_TABS);
+                prev = item.get();
+            }
         }
     }
 }
