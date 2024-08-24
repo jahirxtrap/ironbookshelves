44d2fa433f0a39a3645d7c206748f6327de7ce6e
jahirxtrap <jahirroman2002@gmail.com>
2024-08-24 01:17:22 -0500

    Update config lib

--- neoforge/src/main/java/com/jahirtrap/ironbookshelves/IronbookshelvesMod.java
+++ neoforge/src/main/java/com/jahirtrap/ironbookshelves/IronbookshelvesMod.java
@@ -1,20 +1,16 @@
 package com.jahirtrap.ironbookshelves;
 
+import com.jahirtrap.configlib.TXFConfig;
 import com.jahirtrap.ironbookshelves.init.ModConfig;
 import com.jahirtrap.ironbookshelves.init.ModContent;
-import com.jahirtrap.ironbookshelves.util.configlib.TXFConfig;
 import net.minecraft.world.item.CreativeModeTab.TabVisibility;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.client.ConfigScreenHandler;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
-
-import static com.jahirtrap.ironbookshelves.util.CommonUtils.putAfter;
 
 @Mod(IronbookshelvesMod.MODID)
 public class IronbookshelvesMod {
@@ -23,9 +19,6 @@ public class IronbookshelvesMod {
 
     public IronbookshelvesMod(IEventBus bus) {
         TXFConfig.init(MODID, ModConfig.class);
-        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
-                new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> TXFConfig.getScreen(parent, MODID)));
-
         ModContent.init(bus);
         bus.addListener(this::buildContents);
     }
