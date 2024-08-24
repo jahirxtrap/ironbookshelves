43cd4a09b4523936725c6afdfd3f7d73992cc0ab
jahirxtrap <jahirroman2002@gmail.com>
2024-08-24 01:15:35 -0500

    Update config lib

--- forge/src/main/java/com/jahirtrap/ironbookshelves/IronbookshelvesMod.java
+++ forge/src/main/java/com/jahirtrap/ironbookshelves/IronbookshelvesMod.java
@@ -1,11 +1,9 @@
 package com.jahirtrap.ironbookshelves;
 
+import com.jahirtrap.configlib.TXFConfig;
 import com.jahirtrap.ironbookshelves.init.ModConfig;
 import com.jahirtrap.ironbookshelves.init.ModContent;
-import com.jahirtrap.ironbookshelves.util.configlib.TXFConfig;
-import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -18,9 +16,6 @@ public class IronbookshelvesMod {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
         TXFConfig.init(MODID, ModConfig.class);
-        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
-                new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> TXFConfig.getScreen(parent, MODID)));
-
         ModContent.init(bus);
     }
 }
