3ef9c8c6030ee75b488b1d42f07913545d4b6e8d
jahirxtrap <jahirroman2002@gmail.com>
2024-10-24 19:56:10 -0500

    Update environment, internal changes

--- fabric/src/main/java/com/jahirtrap/ironbookshelves/block/CryingObsidianBookshelfBlock.java
+++ fabric/src/main/java/com/jahirtrap/ironbookshelves/block/CryingObsidianBookshelfBlock.java
@@ -1,32 +1,29 @@
 package com.jahirtrap.ironbookshelves.block;
 
-import com.jahirtrap.ironbookshelves.init.ModConfig;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.PushReaction;
 
 import java.util.Random;
 
 public class CryingObsidianBookshelfBlock extends BaseBookshelfBlock {
-    public CryingObsidianBookshelfBlock() {
-        super(SoundType.STONE, 50f, 1200f, ModConfig.cryingObsidianEnchantPower, 10, PushReaction.BLOCK);
+    public CryingObsidianBookshelfBlock(double power, Properties properties) {
+        super(power, properties);
     }
 
     @Override
-    public void animateTick(BlockState state, Level level, BlockPos pos, Random rand) {
-        if (rand.nextInt(5) == 0) {
-            Direction direction = Direction.getRandom(rand);
+    public void animateTick(BlockState state, Level level, BlockPos pos, Random random) {
+        if (random.nextInt(5) == 0) {
+            Direction direction = Direction.getRandom(random);
             if (direction != Direction.UP) {
                 BlockPos blockpos = pos.relative(direction);
                 BlockState blockstate = level.getBlockState(blockpos);
                 if (!state.canOcclude() || !blockstate.isFaceSturdy(level, blockpos, direction.getOpposite())) {
-                    double d = direction.getStepX() == 0 ? rand.nextDouble() : 0.5 + direction.getStepX() * 0.6;
-                    double e = direction.getStepY() == 0 ? rand.nextDouble() : 0.5 + direction.getStepY() * 0.6;
-                    double f = direction.getStepZ() == 0 ? rand.nextDouble() : 0.5 + direction.getStepZ() * 0.6;
+                    double d = direction.getStepX() == 0 ? random.nextDouble() : 0.5 + direction.getStepX() * 0.6;
+                    double e = direction.getStepY() == 0 ? random.nextDouble() : 0.5 + direction.getStepY() * 0.6;
+                    double f = direction.getStepZ() == 0 ? random.nextDouble() : 0.5 + direction.getStepZ() * 0.6;
                     level.addParticle(ParticleTypes.DRIPPING_OBSIDIAN_TEAR, pos.getX() + d, pos.getY() + e, pos.getZ() + f, 0, 0, 0);
                 }
             }
