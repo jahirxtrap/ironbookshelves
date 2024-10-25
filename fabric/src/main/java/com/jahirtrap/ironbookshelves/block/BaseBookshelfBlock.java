d1e599ffdf4a3ba68a9d98175363aab3de1f5cf6
jahirxtrap <jahirroman2002@gmail.com>
2024-10-24 20:39:58 -0500

    Update environment, internal changes

--- fabric/src/main/java/com/jahirtrap/ironbookshelves/block/BaseBookshelfBlock.java
+++ fabric/src/main/java/com/jahirtrap/ironbookshelves/block/BaseBookshelfBlock.java
@@ -10,10 +10,7 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.MapColor;
-import net.minecraft.world.level.material.PushReaction;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -24,19 +21,18 @@ import static com.jahirtrap.ironbookshelves.util.CommonUtils.formatText;
 public class BaseBookshelfBlock extends Block implements EnchantmentBonusBlock {
     private final float power;
 
-    public BaseBookshelfBlock(SoundType soundType, float hardness, float resistance, double power, int lightLevel, PushReaction reaction) {
-        super(Properties.of().mapColor(MapColor.METAL).sound(soundType).strength(hardness, resistance).requiresCorrectToolForDrops()
-                .lightLevel($ -> lightLevel).pushReaction(reaction));
+    public BaseBookshelfBlock(double power, Properties properties) {
+        super(properties);
         this.power = (float) power;
     }
 
     @Override
-    public float getEnchantPowerBonus(BlockState state, LevelReader world, BlockPos pos) {
+    public float getEnchantPowerBonus(BlockState state, LevelReader level, BlockPos pos) {
         return power;
     }
 
     @Override
-    public void appendHoverText(ItemStack stack, @Nullable BlockGetter world, List<Component> tooltip, TooltipFlag flag) {
+    public void appendHoverText(ItemStack stack, @Nullable BlockGetter level, List<Component> tooltip, TooltipFlag flag) {
         if (ModConfig.enableTooltips && power != 0)
             tooltip.add(coloredTextComponent(Component.translatable("ironbookshelves.bookshelf.power").getString() + formatText(power), ChatFormatting.GRAY));
     }
--- neoforge/src/main/java/com/jahirtrap/ironbookshelves/init/ModContent.java
+++ neoforge/src/main/java/com/jahirtrap/ironbookshelves/init/ModContent.java
@@ -6,8 +6,10 @@ import com.jahirtrap.ironbookshelves.block.CryingObsidianBookshelfBlock;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -21,23 +23,27 @@ public class ModContent {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
 
-    public static final DeferredBlock<Block> IRON_BOOKSHELF = registerBlock("iron_bookshelf", () -> new BaseBookshelfBlock(SoundType.METAL, 5f, 6f, ModConfig.ironEnchantPower, 0, PushReaction.NORMAL), new Item.Properties());
-    public static final DeferredBlock<Block> GOLDEN_BOOKSHELF = registerBlock("golden_bookshelf", () -> new BaseBookshelfBlock(SoundType.METAL, 3f, 6f, ModConfig.goldenEnchantPower, 0, PushReaction.NORMAL), new Item.Properties());
-    public static final DeferredBlock<Block> DIAMOND_BOOKSHELF = registerBlock("diamond_bookshelf", () -> new BaseBookshelfBlock(SoundType.METAL, 5f, 6f, ModConfig.diamondEnchantPower, 0, PushReaction.NORMAL), new Item.Properties());
-    public static final DeferredBlock<Block> EMERALD_BOOKSHELF = registerBlock("emerald_bookshelf", () -> new BaseBookshelfBlock(SoundType.METAL, 5f, 6f, ModConfig.emeraldEnchantPower, 0, PushReaction.NORMAL), new Item.Properties());
-    public static final DeferredBlock<Block> OBSIDIAN_BOOKSHELF = registerBlock("obsidian_bookshelf", () -> new BaseBookshelfBlock(SoundType.STONE, 50f, 1200f, ModConfig.obsidianEnchantPower, 0, PushReaction.BLOCK), new Item.Properties());
-    public static final DeferredBlock<Block> NETHERITE_BOOKSHELF = registerBlock("netherite_bookshelf", () -> new BaseBookshelfBlock(SoundType.NETHERITE_BLOCK, 50f, 1200f, ModConfig.netheriteEnchantPower, 0, PushReaction.NORMAL), new Item.Properties().fireResistant());
-    public static final DeferredBlock<Block> COPPER_BOOKSHELF = registerBlock("copper_bookshelf", () -> new BaseBookshelfBlock(SoundType.COPPER, 3f, 6f, ModConfig.copperEnchantPower, 0, PushReaction.NORMAL), new Item.Properties());
-    public static final DeferredBlock<Block> AMETHYST_BOOKSHELF = registerBlock("amethyst_bookshelf", AmethystBookshelfBlock::new, new Item.Properties());
-    public static final DeferredBlock<Block> CRYING_OBSIDIAN_BOOKSHELF = registerBlock("crying_obsidian_bookshelf", CryingObsidianBookshelfBlock::new, new Item.Properties());
-    public static final DeferredBlock<Block> ENDERITE_BOOKSHELF = registerBlock("enderite_bookshelf", () -> new BaseBookshelfBlock(SoundType.NETHERITE_BLOCK, 66f, 1200f, ModConfig.enderiteEnchantPower, 0, PushReaction.NORMAL), new Item.Properties().fireResistant());
-    public static final DeferredBlock<Block> STEEL_BOOKSHELF = registerBlock("steel_bookshelf", () -> new BaseBookshelfBlock(SoundType.METAL, 5f, 6f, ModConfig.steelEnchantPower, 0, PushReaction.NORMAL), new Item.Properties());
-    public static final DeferredBlock<Block> BRONZE_BOOKSHELF = registerBlock("bronze_bookshelf", () -> new BaseBookshelfBlock(SoundType.METAL, 5f, 6f, ModConfig.bronzeEnchantPower, 0, PushReaction.NORMAL), new Item.Properties());
+    public static final DeferredBlock<Block> IRON_BOOKSHELF = registerBlock("iron_bookshelf", () -> new BaseBookshelfBlock(ModConfig.ironEnchantPower, BlockBehaviour.Properties.ofLegacyCopy(Blocks.IRON_BLOCK)), new Item.Properties());
+    public static final DeferredBlock<Block> GOLDEN_BOOKSHELF = registerBlock("golden_bookshelf", () -> new BaseBookshelfBlock(ModConfig.goldenEnchantPower, BlockBehaviour.Properties.ofLegacyCopy(Blocks.GOLD_BLOCK)), new Item.Properties());
+    public static final DeferredBlock<Block> DIAMOND_BOOKSHELF = registerBlock("diamond_bookshelf", () -> new BaseBookshelfBlock(ModConfig.diamondEnchantPower, BlockBehaviour.Properties.ofLegacyCopy(Blocks.DIAMOND_BLOCK)), new Item.Properties());
+    public static final DeferredBlock<Block> EMERALD_BOOKSHELF = registerBlock("emerald_bookshelf", () -> new BaseBookshelfBlock(ModConfig.emeraldEnchantPower, BlockBehaviour.Properties.ofLegacyCopy(Blocks.EMERALD_BLOCK)), new Item.Properties());
+    public static final DeferredBlock<Block> OBSIDIAN_BOOKSHELF = registerBlock("obsidian_bookshelf", () -> new BaseBookshelfBlock(ModConfig.obsidianEnchantPower, BlockBehaviour.Properties.ofLegacyCopy(Blocks.OBSIDIAN)), new Item.Properties());
+    public static final DeferredBlock<Block> NETHERITE_BOOKSHELF = registerBlock("netherite_bookshelf", () -> new BaseBookshelfBlock(ModConfig.netheriteEnchantPower, BlockBehaviour.Properties.ofLegacyCopy(Blocks.NETHERITE_BLOCK)), new Item.Properties().fireResistant());
+    public static final DeferredBlock<Block> COPPER_BOOKSHELF = registerBlock("copper_bookshelf", () -> new BaseBookshelfBlock(ModConfig.copperEnchantPower, BlockBehaviour.Properties.ofLegacyCopy(Blocks.COPPER_BLOCK)), new Item.Properties());
+    public static final DeferredBlock<Block> AMETHYST_BOOKSHELF = registerBlock("amethyst_bookshelf", () -> new AmethystBookshelfBlock(ModConfig.amethystEnchantPower, BlockBehaviour.Properties.ofLegacyCopy(Blocks.AMETHYST_BLOCK)), new Item.Properties());
+    public static final DeferredBlock<Block> CRYING_OBSIDIAN_BOOKSHELF = registerBlock("crying_obsidian_bookshelf", () -> new CryingObsidianBookshelfBlock(ModConfig.cryingObsidianEnchantPower, BlockBehaviour.Properties.ofLegacyCopy(Blocks.CRYING_OBSIDIAN)), new Item.Properties());
+    public static final DeferredBlock<Block> ENDERITE_BOOKSHELF = registerBlock("enderite_bookshelf", () -> new BaseBookshelfBlock(ModConfig.enderiteEnchantPower, BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).sound(SoundType.NETHERITE_BLOCK).strength(66f, 1200f).requiresCorrectToolForDrops()), new Item.Properties().fireResistant());
+    public static final DeferredBlock<Block> STEEL_BOOKSHELF = registerBlock("steel_bookshelf", () -> new BaseBookshelfBlock(ModConfig.steelEnchantPower, BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GRAY).sound(SoundType.METAL).strength(5f, 6f).requiresCorrectToolForDrops()), new Item.Properties());
+    public static final DeferredBlock<Block> BRONZE_BOOKSHELF = registerBlock("bronze_bookshelf", () -> new BaseBookshelfBlock(ModConfig.bronzeEnchantPower, BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_ORANGE).sound(SoundType.METAL).strength(5f, 6f).requiresCorrectToolForDrops()), new Item.Properties());
 
-    public static DeferredBlock<Block> registerBlock(String name, Supplier<Block> supplier, Item.Properties properties) {
-        var blockReg = BLOCKS.register(name, supplier);
-        registerItem(name, () -> new BlockItem(blockReg.get(), properties));
+    public static DeferredBlock<Block> registerBlock(String name, Supplier<Block> supplier, Item.Properties itemProp) {
+        var blockReg = registerBlock(name, supplier);
+        registerItem(name, () -> new BlockItem(blockReg.get(), itemProp));
         return blockReg;
+    }
+
+    public static DeferredBlock<Block> registerBlock(String name, Supplier<Block> supplier) {
+        return BLOCKS.register(name, supplier);
     }
 
     public static DeferredItem<Item> registerItem(String name, Supplier<Item> supplier) {
