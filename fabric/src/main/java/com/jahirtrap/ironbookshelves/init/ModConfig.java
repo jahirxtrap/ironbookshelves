278502ab2ecb064a942540559ca1d12e36b8c444
jahirxtrap <jahirroman2002@gmail.com>
2024-12-28 02:07:12 -0500

    v1.4.4

--- fabric/src/main/java/com/jahirtrap/ironbookshelves/init/ModConfig.java
+++ fabric/src/main/java/com/jahirtrap/ironbookshelves/init/ModConfig.java
@@ -5,28 +5,28 @@ import com.jahirtrap.configlib.TXFConfig;
 public class ModConfig extends TXFConfig {
     @Entry(name = "Enable Tooltips")
     public static boolean enableTooltips = true;
-    @Entry(name = "Iron Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    @Entry(name = "Iron Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY, itemDisplay = "ironbookshelves:iron_bookshelf")
     public static double ironEnchantPower = 2.0;
-    @Entry(name = "Golden Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    @Entry(name = "Golden Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY, itemDisplay = "ironbookshelves:golden_bookshelf")
     public static double goldenEnchantPower = 3.0;
-    @Entry(name = "Diamond Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    @Entry(name = "Diamond Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY, itemDisplay = "ironbookshelves:diamond_bookshelf")
     public static double diamondEnchantPower = 5.0;
-    @Entry(name = "Emerald Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    @Entry(name = "Emerald Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY, itemDisplay = "ironbookshelves:emerald_bookshelf")
     public static double emeraldEnchantPower = 6.0;
-    @Entry(name = "Obsidian Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    @Entry(name = "Obsidian Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY, itemDisplay = "ironbookshelves:obsidian_bookshelf")
     public static double obsidianEnchantPower = 7.0;
-    @Entry(name = "Netherite Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    @Entry(name = "Netherite Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY, itemDisplay = "ironbookshelves:netherite_bookshelf")
     public static double netheriteEnchantPower = 10.0;
-    @Entry(name = "Copper Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    @Entry(name = "Copper Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY, itemDisplay = "ironbookshelves:copper_bookshelf")
     public static double copperEnchantPower = 1.5;
-    @Entry(name = "Amethyst Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    @Entry(name = "Amethyst Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY, itemDisplay = "ironbookshelves:amethyst_bookshelf")
     public static double amethystEnchantPower = 2.5;
-    @Entry(name = "Crying Obsidian Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    @Entry(name = "Crying Obsidian Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY, itemDisplay = "ironbookshelves:crying_obsidian_bookshelf")
     public static double cryingObsidianEnchantPower = 8.0;
-    @Entry(name = "Enderite Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    @Entry(name = "Enderite Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY, itemDisplay = "ironbookshelves:enderite_bookshelf")
     public static double enderiteEnchantPower = 12.0;
-    @Entry(name = "Steel Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    @Entry(name = "Steel Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY, itemDisplay = "ironbookshelves:steel_bookshelf")
     public static double steelEnchantPower = 3.0;
-    @Entry(name = "Bronze Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY)
+    @Entry(name = "Bronze Enchant Power", min = 0.0, max = Double.POSITIVE_INFINITY, itemDisplay = "ironbookshelves:bronze_bookshelf")
     public static double bronzeEnchantPower = 4.0;
 }
