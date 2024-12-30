59c85cb0f9f2f0bd54a31b6492371d117f38dcf5
jahirxtrap <jahirroman2002@gmail.com>
2024-12-30 04:54:08 -0500

    Remove tooltips

--- forge/src/main/java/com/jahirtrap/ironbookshelves/block/BaseBookshelfBlock.java
+++ forge/src/main/java/com/jahirtrap/ironbookshelves/block/BaseBookshelfBlock.java
@@ -1,22 +1,9 @@
 package com.jahirtrap.ironbookshelves.block;
 
-import com.jahirtrap.ironbookshelves.init.ModConfig;
-import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
-
-import static com.jahirtrap.ironbookshelves.util.CommonUtils.coloredTextComponent;
-import static com.jahirtrap.ironbookshelves.util.CommonUtils.formatText;
 
 public class BaseBookshelfBlock extends Block {
     private final float power;
@@ -29,11 +16,5 @@ public class BaseBookshelfBlock extends Block {
     @Override
     public float getEnchantPowerBonus(BlockState state, LevelReader level, BlockPos pos) {
         return power;
-    }
-
-    @Override
-    public void appendHoverText(ItemStack stack, @Nullable BlockGetter level, List<Component> tooltip, TooltipFlag flag) {
-        if (ModConfig.enableTooltips && power != 0)
-            tooltip.add(coloredTextComponent(new TranslatableComponent("ironbookshelves.bookshelf.power").getString() + formatText(power), ChatFormatting.GRAY));
     }
 }
